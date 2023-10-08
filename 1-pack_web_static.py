4d424d59667f937525fe8ce744f94dc0025ea964
belyne <nzabonamarie@gmail.com>
2023-10-08 12:15:36 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
 
 from fabric.api import local
 from datetime import datetime
+import os
 
 def do_pack():
     try:
@@ -11,6 +12,12 @@ def do_pack():
         date_format = now.strftime("%Y%m%d%H%M%S")
         archive_name = "versions/web_static_{}.tgz".format(date_format)
         local("tar -cvzf {} web_static".format(archive_name))
-        return archive_name
-    except:
+        
+        # Check if the archive was created successfully
+        if os.path.exists(archive_name):
+            return archive_name
+        else:
+            return None
+    except Exception as e:
+        print(e)
         return None
